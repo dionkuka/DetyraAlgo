5a1b89f3f14f6327763389f9de6a3d2cffe55abb
Dev 3 <dion.kuka@ubt-uni.net>
2022-07-02 23:00:14 +0200

    Constraints added

--- DetyraDioni.java
+++ DetyraDioni.java
@@ -4,10 +4,25 @@ public class DetyraDioni {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Bob's money: ");
 		int x = scan.nextInt(); // Amount of money Bob has
+		while(x < 1 || x > 10){
+			System.out.println("Bob's amount of money should be between 1 and 10 !!");
+			System.out.print("Bob's money: ");
+			x = scan.nextInt();
+		}
 		System.out.print("Price of 1 kg apples: ");
 		int a = scan.nextInt(); // Cost per kg of apples
+		while(a < 1 || a > 10){
+			System.out.println("Price of apples can't be higher than 10 or less than 1 !!");
+			System.out.print("Price of 1 kg apples: ");
+			a = scan.nextInt();
+		}
 		System.out.print("Price of 1 kg oranges: ");
 		int b = scan.nextInt(); // Cost per kg of oranges
+		while(b < 1 || b > 10){
+			System.out.println("Price of oranges can't be higher than 10 or less than 1 !!");
+			System.out.print("Price of 1 kg oranges: ");
+			b = scan.nextInt();
+		}
 		System.out.println("--------------------------------");
 		System.out.println(x); // Printing amount of money Bob has
 		System.out.println(a+" "+b); // Printing prices of apples and oranges
