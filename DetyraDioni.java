bcf9ab9cbc70d164e415e0723ab740a7a1ec5571
Dev 3 <dion.kuka@ubt-uni.net>
2022-07-02 22:30:52 +0200

    First Commit

---
+++ DetyraDioni.java
@@ -0,0 +1,8 @@
+public class DetyraDioni {
+	public static void main(String [] args){
+		int x = 10; // Amount of money Bob has
+		int a = 5; // Cost per kg of apples
+		int b = 4; // Cost per kg of oranges
+		
+	}
+}
