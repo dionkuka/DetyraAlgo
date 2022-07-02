c67b339885a16dfad57d6ac72f69391d2dbd1477
Dev 3 <dion.kuka@ubt-uni.net>
2022-07-02 22:48:46 +0200

    Scanner added and calculations finished

--- DetyraDioni.java
+++ DetyraDioni.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 public class DetyraDioni {
 	public static void main(String [] args){
-		int x = 10; // Amount of money Bob has
-		int a = 5; // Cost per kg of apples
-		int b = 4; // Cost per kg of oranges
-		System.out.println("Dioni");	
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Bob's money: ");
+		int x = scan.nextInt(); // Amount of money Bob has
+		System.out.print("Price of 1 kg apples: ");
+		int a = scan.nextInt(); // Cost per kg of apples
+		System.out.print("Price of 1 kg oranges: ");
+		int b = scan.nextInt(); // Cost per kg of oranges
+		System.out.println("--------------------------------");
+		System.out.println(x); // Printing amount of money Bob has
+		System.out.println(a+" "+b); // Printing prices of apples and oranges
+		int price = a + b; // Price of 1 kg apple and orange
+		if(x >= price){ 
+			System.out.println("Yes"); // if Bob's amount of money is equal or higher than price of 1 kg apple and orange we print Yes
+		}else{
+			System.out.println("No");
+		}
 	}
 }
